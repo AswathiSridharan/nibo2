c0c5d17bd127b0f165d591aaf65c23cd84c1f07b
Victor Ruch <viruch3112@gmail.com>
2012-06-11 11:36:37 +0200

    prf P3d Nibo2 files added

--- main.c
+++ main.c
@@ -10,9 +10,9 @@
 #include "nibo/pwm.h"
 #include "nibo/bot.h"
 
-#include "proximitysensors.h"
 #include "mylog.h"
-#include "stayonline.h"
+#include "blink.h"
+#include "turn.h"
 
 #include <avr/interrupt.h>
 #include <avr/pgmspace.h>
@@ -22,23 +22,12 @@
 #define LOBYTE(x)        (uint8_t)((uint16_t)x)
 #define HIBYTE(x)        (uint8_t)(((uint16_t)x)>>8)
 #define MAKE_WORD(hi,lo) ((hi*0x100)+lo)
-
-int16_t tspeed_l = 0;
-int16_t tspeed_r = 0;
+#define MAX_DISTANCE 0xe5
 
 uint8_t dist[5];
+uint8_t turns = 0;
 
-uint8_t dist_max_ls; // max leftside
-uint8_t dist_max_fs; // max frontside
-uint8_t dist_max_rs; // max rightside
-
-uint8_t headlight_target;
-uint8_t headlight_act;
-
-float volt_flt = 10.0;
-
-uint16_t stress;
-
+// prints int as hex 
 void print_hex (uint8_t val) {
   char c1=val/16;
   char c2=val%16;
@@ -48,7 +37,19 @@ void print_hex (uint8_t val) {
   gfx_print_char(c2);
 }
 
-uint8_t back_cnt;
+// Logs the distance from all proximity sensors in hex.
+void log_distance() {
+    gfx_move(25, 20);
+    print_hex(dist[0]);
+    gfx_print_char(' ');
+    print_hex(dist[1]);
+    gfx_print_char(' ');
+    print_hex(dist[2]);
+    gfx_print_char(' ');
+    print_hex(dist[3]);
+    gfx_print_char(' ');
+    print_hex(dist[4]);
+}
 
 int main(void) {
   sei(); // enable interrupts
@@ -71,10 +72,6 @@ int main(void) {
   //motco_setSpeedParameters(5, 4, 6); // ki, kp, kd
   copro_setSpeedParameters(15, 20, 10); // ki, kp, kd
 
-  stress = 0;
-
-  //drive_and_stay_on_line();
-  
   while (1) {
     delay(50);
     leds_set_displaylight(1024);
@@ -82,86 +79,42 @@ int main(void) {
     // Spannung
     bot_update();
     
-    // Analog values:
-    floor_update();
-    int16_t value_fl = floor_l;
-    int16_t value_fr = floor_r;
-    int16_t value_ll = line_l;
-    int16_t value_lr = line_r;
-
-    if (value_fl<0) value_fl=0;
-    if (value_fr<0) value_fr=0;
-    if (value_ll<0) value_ll=0;
-    if (value_lr<0) value_lr=0;
-    if (value_fl>255) value_fl=255;
-    if (value_fr>255) value_fr=255;
-    if (value_ll>255) value_ll=255;
-    if (value_lr>255) value_lr=255;
-
-    gfx_move(30, 10);
-    print_hex(value_fr);
-    gfx_print_char(' ');
-    print_hex(value_lr);
-    gfx_print_char(' ');
-    print_hex(value_ll);
-    gfx_print_char(' ');
-    print_hex(value_fl);
-
-    // Distance
-
-    int16_t speed_l = 0;
-    int16_t speed_r = 0;
-
     // Request distance data
-    if (!copro_update()) {
-      gfx_move(10, 10);
-      gfx_set_proportional(1);
-      gfx_print_text("COPRO Error   ");
-      gfx_set_proportional(0);
-      continue;
-    }
+    copro_update();
 
-    dist[4] = copro_distance[4]/128;
-    dist[3] = copro_distance[3]/128;
-    dist[2] = copro_distance[2]/128;
-    dist[1] = copro_distance[1]/128;
-    dist[0] = copro_distance[0]/128;
+    dist[4] = copro_distance[4]/128; // left
+    dist[3] = copro_distance[3]/128; // front left
+    dist[2] = copro_distance[2]/128; // front 
+    dist[1] = copro_distance[1]/128; // front right
+    dist[0] = copro_distance[0]/128; // right
 
     dist[4] = (dist[4]<250)? (dist[4]+5):255;
     dist[0] = (dist[0]<250)? (dist[0]+5):255;
     dist[2] = (dist[2]>5)? (dist[2]-5):0;
 
-
-    dist_max_ls = (dist[4]>dist[3])?dist[4]:dist[3];
-    dist_max_rs = (dist[0]>dist[1])?dist[0]:dist[1];
-    dist_max_fs = (dist[1]>dist[3])?dist[1]:dist[3];
-    if (dist[2]>dist_max_fs) dist_max_fs = dist[2];
-
-    gfx_move(25, 20);
-    print_hex(dist[0]);
-    gfx_print_char(' ');
-    print_hex(dist[1]);
-    gfx_print_char(' ');
-    print_hex(dist[2]);
-    gfx_print_char(' ');
-    print_hex(dist[3]);
-    gfx_print_char(' ');
-    print_hex(dist[4]);
-
-    proximity_check(dist);
+    log_distance();
     
-    uint16_t sum = dist[0] + dist[1]
-                 + dist[2] + dist[3]
-                 + dist[4];
-    sum /= 5;
-    
-    uint8_t dmax = (sum<0xe0)?(sum+0x20):0xff;
-    uint8_t dmin = (sum>0x20)?(sum-0x20):0x00;
+    if(dist[2] >= MAX_DISTANCE){  // if wall in front
+      turns = turns + 1;
+      if(dist[1] < dist[3]){ // if wall is closer to front left sensor
+        turn_right();
+      } else {  // or closer to front right sensor
+        turn_left();
+      }
+    } else if (dist[1] >= MAX_DISTANCE) { // if too close to front right sensor
+      turns = turns + 1;
+      turn_left();
+    } else if (dist[3] >= MAX_DISTANCE) { // if too close to front left sensor
+      turns = turns + 1;
+      turn_right();
+    } else { // else just drive straight on
+      copro_setSpeed(10,10);
+    }
 
-   
-    stress = (stress>10)?(stress-10):(0);
-
-    stress += copro_current_l;
-    stress += copro_current_r;
+    if(turns >= 4) { // if 4 turns have been made
+      turns = 0;
+      // dance crazy
+      blink();
+    }
   }
 }
